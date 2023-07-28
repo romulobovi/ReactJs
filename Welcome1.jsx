7f52396051426e90739e875b14f43c77759eb826
Rômulo Bovi <58385939+romulobovi@users.noreply.github.com>
2023-07-28 11:01:31 -0300

    welcome1.jsx

---
+++ Welcome1.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function App() {
+  return (
+    <div>
+      <h1>Bem-vindo ao meu site!</h1>
+      <p>Este é um exemplo de código ReactJS.</p>
+    </div>
+  );
+}
+
+export default App;
